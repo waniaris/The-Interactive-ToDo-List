db126d5651aa36f06c28a33514da759994463fd1
waniaris <norhazwaniaris@gmail.com>
2025-06-28 14:21:23 +0100

    add duplicate check code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,36 @@ var addButton = document.getElementById("addButton");
 
 function onClickAddButton (){
     var taskInputValue = newTaskInput.value.trim();
-    if (taskInputValue !== ""){ // !== "" ensure the input is not an empty string.
-        var newTaskE1 = document.createElement("li"); // display the task in the task list.
-        newTaskE1.textContent = taskInputValue; // Sets the content of the new list item to whatever the user typed
-        taskList.appendChild(newTaskE1); // Adds the new <li> to the task list (<ul> element 
-        newTaskInput.value = ""; // clear input after adding
+
+    // check if input is empty
+    if (taskInputValue === ""){
+        errorMsg.textContent = "Oopps, empty input. Please type in a task.";
+        errorMsg.style.display = "block";
+        return;
     }
+
+    // check if input is duplicate
+    var isDuplicate = false;
+    var existingInput = document.getElementsByTagName("li");
+
+    for (var i = 0; i < existingInput.length; i++) {
+        if (existingInput[i].textContent === taskInputValue) {
+            isDuplicate = true;
+            break;
+        }
+    }
+    if (isDuplicate) {
+        errorMsg.textContent = "This task already exists. Please review your input and check task list.";
+        errorMsg.style.display = "block";
+        return;
+    }
+
+    // if all valid:
+    var newTaskE1 = document.createElement("li"); // display the task in the task list.
+    newTaskE1.textContent = taskInputValue.trim(); // Sets the content of the new list item to whatever the user typed
+    taskList.appendChild(newTaskE1); // Adds the new <li> to the task list (<ul> element 
+    newTaskInput.value = ""; // clear input after adding
+    errorMsg.style.display = "none";
 }
 
 addButton.addEventListener("click", onClickAddButton);
